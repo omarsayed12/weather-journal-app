0072f530e04ef97c75d394e723a32b12ee813631
omarsayed12 <omarsayed40400@gmail.com>
2021-12-13 12:40:30 +0200

    setup server and npm packages

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+// define port
+let port = 6000;
+
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express(); 
 
 /* Middleware*/
+
+// define cors and body-parser
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 
 // Setup Server
+app.listen(port, () => {
+    console.log(`server will running on port ${port}`);
+})
